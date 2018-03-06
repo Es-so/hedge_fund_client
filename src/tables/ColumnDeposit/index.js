525edd844c1a728e1f0c672fbe9c1b281a9b7d59
vbraeke <miku2s@MacBook-Pro-de-Braeke.local>
2018-03-06 20:26:19 +0100

    tables

---
+++ src/tables/ColumnDeposit/index.js
@@ -0,0 +1,26 @@
+export const columns_deposit = [ 
+	{
+		  title: 'Amount',
+		  dataIndex: 'amount',
+		  height:50,
+		  width: 20,
+	},
+	{
+		  title: 'Currency',
+		  dataIndex: 'currency',
+		  height:50,
+		  width: 20,
+	},  
+	{
+		  title: 'Dollar',
+		  dataIndex: 'amount_dollar',
+		  height:50,
+		  width: 20,
+	},
+	{
+		  title: 'Date',
+		  dataIndex: 'date',
+		  height:50,
+		  width: 25,
+	}
+];
---
+++ src/tables/ColumnPositions/index.js
@@ -0,0 +1,245 @@
+export const columnsOpen = [ 
+	{
+	  title: 'ID',
+	  dataIndex: 'id',
+	  height:30,
+	  width: 40,
+	},{
+	  title: 'Base currency',
+	  dataIndex: 'base_currency',
+	  height:30,
+	  width: 40,
+	},{
+	  title: 'Target currency',
+	  dataIndex: 'target_currency',
+	  height:30,
+	  width: 40,
+	},  {
+	  title: 'Amount base currency',
+	  dataIndex: 'amount_base',
+	  height:30,
+	  width: 40,
+	},{
+	  title: 'Amount target currency',
+	  dataIndex: 'amount_target',
+	  height:30,
+	  width: 40,
+	},{
+	  title: 'Base price buy',
+	  dataIndex: 'base_price_buy',
+	  height:30,
+	  width: 40,
+	},{
+	  title: 'Dollar invested',
+	  dataIndex: 'invested_dollar',
+	  height:30,
+	  width: 40,
+	},{
+	  title: 'Current balance',
+	  dataIndex: 'current_balance',
+	  height:30,
+	  width: 40,
+	},{
+	  title: 'Date of purchase',
+	  dataIndex: 'date',
+	  height:30,
+	  width: 40,
+	},{
+	  title: 'Exchange',
+	  dataIndex: 'exchange',
+	  height:30,
+	  width: 40,
+	}
+];
+
+export const columnsClose = [ 
+	{
+	  title: 'ID',
+	  dataIndex: 'id',
+	  height:30,
+	  width: 40,
+	},{
+	  title: 'Base currency',
+	  dataIndex: 'base_currency',
+	  height:30,
+	  width: 40,
+	},{
+	  title: 'Target currency',
+	  dataIndex: 'target_currency',
+	  height:30,
+	  width: 40,
+	},  {
+	  title: 'Price base currency',
+	  dataIndex: 'price_base_currency',
+	  height:30,
+	  width: 40,
+	},{
+	  title: 'Price target currency',
+	  dataIndex: 'price_target_currency',
+	  height:30,
+	  width: 40,
+	},{
+	  title: 'Quantity',
+	  dataIndex: 'quantity',
+	  height:30,
+	  width: 40,
+	},{
+	  title: 'Close date',
+	  dataIndex: 'date',
+	  height:30,
+	  width: 40,
+	}
+];
+
+export const columnsStratClose = [ 
+	{
+	  title: 'id_event',
+	  dataIndex: 'id_event',
+	  height:30,
+	  width: 40,
+	},{
+	  title: 'Base',
+	  dataIndex: 'base_currency',
+	  height:30,
+	  width: 40,
+	},{
+	  title: 'Target',
+	  dataIndex: 'target_currency',
+	  height:30,
+	  width: 40,
+	},{
+	  title: 'Base price O',
+	  dataIndex: 'buy_btc_price',
+	  height:30,
+	  width: 40,
+	},  {
+	  title: 'Base dollar price O',
+	  dataIndex: 'buy_dollar_price',
+	  height:30,
+	  width: 40,
+	}, {
+	  title: 'Base price C',
+	  dataIndex: 'sell_btc_price',
+	  height:30,
+	  width: 40,
+	}, {
+	  title: 'Base dollar price C',
+	  dataIndex: 'sell_dollar_price',
+	  height:30,
+	  width: 40,
+	},{
+	  title: 'Dollar invested',
+	  dataIndex: 'invested',
+	  height:30,
+	  width: 40,
+	},{
+	  title: 'Quantity',
+	  dataIndex: 'quantity',
+	  height:30,
+	  width: 40,
+	},{
+	  title: 'Purchase date',
+	  dataIndex: 'open_date',
+	  height:30,
+	  width: 40,
+	},{
+	  title: 'Future sell date',
+	  dataIndex: 'closed_date',
+	  height:30,
+	  width: 40,
+	},{
+	  title: 'pnl',
+	  dataIndex: 'pnl',
+	  height:30,
+	  width: 40,
+	}
+];
+
+export const columnsStratOpen = [ 
+	{
+	  title: 'id_event',
+	  dataIndex: 'id_event',
+	  height:30,
+	  width: 40,
+	},{
+	  title: 'Base',
+	  dataIndex: 'base_currency',
+	  height:30,
+	  width: 40,
+	},{
+	  title: 'Target',
+	  dataIndex: 'target_currency',
+	  height:30,
+	  width: 40,
+	},{
+	  title: 'Base price',
+	  dataIndex: 'buy_btc_price',
+	  height:30,
+	  width: 40,
+	},  {
+	  title: 'Base dollar',
+	  dataIndex: 'buy_dollar_price',
+	  height:30,
+	  width: 40,
+	},{
+	  title: 'Dollar invested',
+	  dataIndex: 'invested',
+	  height:30,
+	  width: 40,
+	},{
+	  title: 'Quantity',
+	  dataIndex: 'quantity',
+	  height:30,
+	  width: 40,
+	},{
+	  title: 'Purchased',
+	  dataIndex: 'open_date',
+	  height:30,
+	  width: 40,
+	},{
+	  title: 'Close',
+	  dataIndex: 'closed_date',
+	  height:30,
+	  width: 40,
+	}
+];
+
+export const columnsSellInfos = [ 
+	{
+	  title: 'ID',
+	  dataIndex: 'id',
+	  height:30,
+	  width: 40,
+	},{
+	  title: 'Base',
+	  dataIndex: 'base_currency',
+	  height:30,
+	  width: 40,
+	},{
+	  title: 'Target',
+	  dataIndex: 'target_currency',
+	  height:30,
+	  width: 40,
+	},{
+	  title: 'Price base currency',
+	  dataIndex: 'price_base_currency',
+	  height:30,
+	  width: 40,
+	},  {
+	  title: 'Price target currency',
+	  dataIndex: 'price_target_currency',
+	  height:30,
+	  width: 40,
+	},{
+	  title: 'Quantity',
+	  dataIndex: 'quantity',
+	  height:30,
+	  width: 40,
+	},{
+	  title: 'Sell date',
+	  dataIndex: 'date',
+	  height:30,
+	  width: 40,
+	}
+];
+
